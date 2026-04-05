f95b14706ca1e856e50aa72dc351e6a5134f3f8d
dpelizzari087-lab <dpelizzari087@gmail.com>
2026-04-05 11:08:17 +0200

    Replace body-parser with express built-in methods

--- api/oauth.js
+++ api/oauth.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const fetch = require("node-fetch");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // CONFIG ALEXA
 const CLIENT_ID = "stiga-client-id";
